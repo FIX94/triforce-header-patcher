a40ddeab0748b42ae1519f2017d03b79e9c51394
Zopolis4 <creatorsmithmdt@gmail.com>
2021-10-19 19:57:40 +1100

    Version 1.5a (Add support for more revisions, correctly name Virtua Striker 3, rewrite readme)

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
+# About
+This tool is designed to patch Triforce games for use with [Nintendont](https://github.com/FIX94/Nintendont), by editing their header. 
+Do not use this tool if you are going to use [Dolphin](https://github.com/dolphin-emu/dolphin).
+
 # Usage
-Grab the [TriHdrPatcher.exe](bin/TriHdrPatcher.exe?raw=true).  
-Drag and drop whatever file you might have into the executable. If they are still in MAME CHD/BIN format make sure to use [triforce-iso-extract](https://github.com/FIX94/triforce-iso-extract) on them first.   
-The tool will inform you if the file has a known SHA1, will fix some known dump errors and of course set a nice header for your loader. If you are using Nintendont as loader you can rename your patched file to game.iso afterwards.   
+Download the binary for your operating system from the Releases section.
+
+Drag and drop your decrypted triforce image onto the executable. 
+If your file is in another format, use [this](https://github.com/FIX94/triforce-iso-extract) on it first.   
 As of right now the following games will get these IDs and game titles:  
 GFZJ8P - F-Zero AX  
 GGPE01 - Mario Kart Arcade GP  
 GGPE02 - Mario Kart Arcade GP 2  
 GPBJ8P - Gekitou Pro Yakyuu  
-GVS32J - Virtua Striker 3 Ver. 2002  
-GVS32E - Virtua Striker 3 Ver. 2002  
+GVS32J - Virtua Striker 3  
+GVS32E - Virtua Striker 3  
 GVS45J - Virtua Striker 4  
 GVS45E - Virtua Striker 4  
 GVS46J - Virtua Striker 4 Ver. 2006  
@@ -16,10 +21,9 @@ GVS46E - Virtua Striker 4 Ver. 2006
 
 # Manual Compiling
 
-## windows
-~~As of right now this is a windows only code~~. If you have MinGW installed and gcc referenced in your PATH variable just use the "build.bat".  
-Support for other OSes might follow in the future.
+## Windows
+If you have MinGW installed and gcc referenced in your PATH variable, clone the repo and run build.bat.  
 
-## linux
-run build.sh, does the same thing as build.bat on windows
+## Linux
+Clone the repo and run build.sh.
 
--- main.c
+++ main.c
@@ -23,7 +23,6 @@
 #include "staticVals.h"
 #include "good_tex_gp2.h"
 
-
 int chSize( int fd, long size )
 {
   #ifdef _WIN32
@@ -32,8 +31,6 @@ int chSize( int fd, long size )
     return ftruncate(fd, size);
   #endif
 }
-
-
 void printerr(char *msg)
 {
 	puts(msg);
@@ -49,7 +46,6 @@ void handleIso(FILE *f, const unsigned char *wantHdr)
 		exit(-1);
 	}
 	fseek(f,0,SEEK_SET);
-
 	unsigned char gameHdr[0x40];
 	fread(gameHdr,0x40,1,f);
 	if(memcmp(gameHdr,wantHdr,0x40) == 0)
@@ -98,7 +94,6 @@ void getBodySha1(FILE *f, size_t fsize, void *chksum)
 	size_t i;
 	SHA1_CONTEXT fSha1;
 	sha1_init(&fSha1);
-
 	unsigned char *fbuf = malloc(0x1000000);
 	fseek(f, 0x40, SEEK_SET); //doing body only
 	for(i = 0x40; i < fsize; i += 0x1000000)
@@ -108,7 +103,6 @@ void getBodySha1(FILE *f, size_t fsize, void *chksum)
 		sha1_write(&fSha1, fbuf, readsize);
 	}
 	free(fbuf);
-
 	sha1_final(&fSha1);
 	memcpy(chksum, fSha1.buf, 20);
 
@@ -169,10 +163,10 @@ void printUnkChksum(unsigned char *chksum)
 
 int main(int argc, char *argv[])
 {
-	puts("Triforce Header Patcher v1.4[kam0de] by FIX94");
+	puts("Triforce Header Patcher v1.5 by FIX94, fro0xwm and Zopolis4");
 	if(argc != 2)
 	{
-		printerr("Please drag and drop a file into this exe.");
+		printerr("Please drag and drop a file into this window.");
 		return -1;
 	}
 	FILE *f = fopen(argv[1],"rb+");
@@ -188,8 +182,9 @@ int main(int argc, char *argv[])
 	{
 		puts("Guessing F-Zero AX");
 		getBodySha1(f, 0x19FCC500, chksum);
-		if(isSha1of(chksum, ax_4c_chksum, "F-Zero AX [GDT-0004C]") || isSha1of(chksum, ax_4d_chksum, "F-Zero AX [GDT-0004D]")
-		 || isSha1of(chksum, ax_4e_chksum, "F-Zero AX [GDT-0004E]"))
+		if(isSha1of(chksum, ax_4c_chksum, "F-Zero AX (Rev C) [GDT-0004C]")
+		 || isSha1of(chksum, ax_4d_chksum, "F-Zero AX (Rev D) [GDT-0004D]")
+		 || isSha1of(chksum, ax_4e_chksum, "F-Zero AX (Rev E) [GDT-0004E]"))
 			handleIso(f, axHdr);
 		else
 			printUnkChksum(chksum);
@@ -243,30 +238,42 @@ int main(int argc, char *argv[])
 	{
 		puts("Guessing Gekitou Pro Yakyuu");
 		getBodySha1(f, 0x1EF00000, chksum);
-		if(isSha1of(chksum, gpb_8b_chksum, "Gekitou Pro Yakyuu [GDT-0008B]") || isSha1of(chksum, gpb_8c_chksum, "Gekitou Pro Yakyuu [GDT-0008C]"))
+		if(isSha1of(chksum, gpb_8b_chksum, "Gekitou Pro Yakyuu (Rev B) [GDT-0008B]")
+			|| isSha1of(chksum, gpb_8c_chksum, "Gekitou Pro Yakyuu (Rev C) [GDT-0008C]"))
 			handleIso(f, gpbHdr);
 		else
 			printUnkChksum(chksum);
 	}
 	else if(fsize == 0x1262C0A8)
 	{
-		puts("Guessing Virtua Striker 3 Ver. 2002");
+		puts("Guessing Virtua Striker 2002");
 		getBodySha1(f, 0x1262C0A8, chksum);
-		if(isSha1of(chksum, vs3v02jap_1_chksum, "Virtua Striker 3 Ver. 2002 (Japan) [GDT-0001]"))
-			handleIso(f, vs3v02japHdr);
-		else if(isSha1of(chksum, vs3v02exp_2_chksum, "Virtua Striker 3 Ver. 2002 (Export) [GDT-0002]"))
-			handleIso(f, vs3v02expHdr);
+		if(isSha1of(chksum, vs02jap_1_chksum, "Virtua Striker 2002 (Japan) [GDT-0001]"))
+			handleIso(f, vs02japHdr);
+		else if(isSha1of(chksum, vs02exp_2_chksum, "Virtua Striker 2002 (Export) [GDT-0002]"))
+			handleIso(f, vs02expHdr);
 		else
 			printUnkChksum(chksum);
 	}
+	else if(fsize == 0x1262A748)
+	{
+		puts("Guessing Virtua Striker 2002");
+		getBodySha1(f, 0x1262A748, chksum);
+		if(isSha1of(chksum, vs02tp3_12_chksum, "Virtua Striker 2002 (Type 3) [GDT-0012]"))
+			handleIso(f, vs02expHdr);
+		else
+			printUnkChksum(chksum);
+	}	
 	else if(fsize == 0x1CA1A400)
 	{
 		puts("Guessing Virtua Striker 4");
 		getBodySha1(f, 0x1CA1A400, chksum);
-		if(isSha1of(chksum, vs4jap_13e_chksum, "Virtua Striker 4 (Japan) [GDT-0013E]"))
+		if(isSha1of(chksum, vs4jap_13e_chksum, "Virtua Striker 4 (Japan, Rev E) [GDT-0013E]"))
 			handleIso(f, vs4japHdr);
-		else if(isSha1of(chksum, vs4exp_14_chksum, "Virtua Striker 4 (Export) [GDT-0014]") ||
-				isSha1of(chksum, vs4exp_15_chksum, "Virtua Striker 4 (Export) [GDT-0015]"))
+		else if(isSha1of(chksum, vs4asi_14_chksum, "Virtua Striker 4 (Asia) [GDT-0014]")
+			  || isSha1of(chksum, vs4asi_14b_chksum, "Virtua Striker 4 (Asia, Rev B) [GDT-0014B]")
+				|| isSha1of(chksum, vs4exp_15_chksum, "Virtua Striker 4 (Export) [GDT-0015]")
+				|| isSha1of(chksum, vs4exp_15a_chksum, "Virtua Striker 4 (Export, Rev A) [GDT-0015A]"))
 			handleIso(f, vs4expHdr);
 		else
 			printUnkChksum(chksum);
@@ -275,7 +282,7 @@ int main(int argc, char *argv[])
 	{
 		puts("Guessing Virtua Striker 4 (Overdump 1)");
 		getBodySha1(f, 0x1CA1A400, chksum);
-		if(isSha1of(chksum, vs4jap_13e_chksum, "Virtua Striker 4 (Japan) [GDT-0013E]"))
+		if(isSha1of(chksum, vs4jap_13e_chksum, "Virtua Striker 4 (Japan, Rev E) [GDT-0013E]"))
 		{
 			chSize(fileno(f), 0x1CA1A400); //windows only bleh
 			puts("Adjusted filesize!");
@@ -295,8 +302,8 @@ int main(int argc, char *argv[])
 		puts("Guessing Virtua Striker 4 Ver. 2006 (Japan)");
 		fixVs4v06Jap(f); // important to do before SHA1
 		getBodySha1(f, 0x1D4130C8, chksum);
-		if(isSha1of(chksum, vs4v06jap_20b_chksum, "Virtua Striker 4 Ver. 2006 (Japan) [GDT-0020B]") ||
-			isSha1of(chksum, vs4v06jap_20d_chksum, "Virtua Striker 4 Ver. 2006 (Japan) [GDT-0020D]"))
+		if(isSha1of(chksum, vs4v06jap_20b_chksum, "Virtua Striker 4 Ver. 2006 (Japan, Rev B) [GDT-0020B]") ||
+			isSha1of(chksum, vs4v06jap_20d_chksum, "Virtua Striker 4 Ver. 2006 (Japan, Rev D) [GDT-0020D]"))
 			handleIso(f, vs4v06japHdr);
 		else
 			printUnkChksum(chksum);
